df1bfa3130a0659654f831d38da69612773adf9d
FirozHossain82 <firozhossain295@gmail.com>
2022-11-14 00:11:43 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Header.css"
+const Header = () => {
+    return (
+        <div>
+                <h1>Welcome to My Ema John Website</h1>
+        </div>
+    );
+};
+
+export default Header;
