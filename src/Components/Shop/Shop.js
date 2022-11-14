d3b3f5412947bd6c01d7de4d7b57875a59340a1d
FirozHossain82 <firozhossain295@gmail.com>
2022-11-14 14:17:50 +0600

    third commit

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import "./Shop.css"
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect( ()=>{
         fetch('products.json')
         .then(res =>res.json())
         .then(data => setProducts(data))
     },[])
+    const handleAddToCart = (product)=>{
+                const newCart =[...cart, product];
+                setCart(newCart);
+    }
     return (
         <div className='shop-container'>
-            <div className="Products-container">
-                    <h3>This is for Products:{products.length}</h3>
+            <div className="products-container">
+                    {
+                        products.map(product => <Product 
+                             key={product.id}
+                             product ={product}
+                             handleAddToCart = {handleAddToCart}
+                             ></Product>)
+                    }
             </div>
 
             <div className="cart-container">
-                        <h4>Order summary</h4>
+                        <h2>Order summary</h2>
+                        <p>Selected Items: {cart.length}</p>
+
             </div>
         </div>
     );
